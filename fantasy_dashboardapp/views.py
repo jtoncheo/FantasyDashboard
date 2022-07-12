59d5c5a6edf7cd81005fd3799c91992986185a48
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-11 19:45:12 -0500

    Created nested pages dependent on a view

--- fantasy_dashboardapp/views.py
+++ fantasy_dashboardapp/views.py
@@ -12,3 +12,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'fantasy_dashboardapp/topics.html', context)
+
+def topic(request, topic_id):
+    '''Show just a single topic'''
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'fantasy_dashboardapp/topic.html', context)
+    
