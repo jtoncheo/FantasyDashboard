8814a48aa3fda912544220c4713224ca79890030
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-11 19:25:23 -0500

    Created a base html template and added a new page

--- fantasy_dashboardapp/urls.py
+++ fantasy_dashboardapp/urls.py
@@ -7,5 +7,7 @@ from . import views
 app_name = 'fantasy_dashboardapp'
 urlpatterns = [
     #Landing Page - Home Page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #Page that shows all of the topics 
+    path('topics/', views.topics, name='topics'),
 ]
--- fantasy_dashboardapp/views.py
+++ fantasy_dashboardapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     '''Creating the home page for fantasy dashboard.'''
     return render(request, 'fantasy_dashboardapp/index.html')
+
+def topics(request):
+    '''Show all topics.'''
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'fantasy_dashboardapp/topics.html', context)
