6491425793700b9da143d0ebf7d8e9740ea88da8
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-21 22:23:03 -0500

    Added user registration

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     #Include the default auth urls
     path('', include('django.contrib.auth.urls')),
+    #Adding the registration page
+    path('register/', views.register, name='register'),
 ]
