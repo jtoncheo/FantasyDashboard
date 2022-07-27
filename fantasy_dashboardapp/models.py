548e0ffa4887191342a6ceba46de9b91fbfc3230
Jesus Tonche <jesustoncheochoa@gmail.com>
2022-07-26 22:14:59 -0500

    Attached views to specific users

--- fantasy_dashboardapp/models.py
+++ fantasy_dashboardapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 '''This will be repurposed later to hold topic information for the website'''
@@ -6,6 +7,7 @@ class Topic(models.Model):
 
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         '''This helper method returns a string of the model'''
--- fantasy_dashboardapp/views.py
+++ fantasy_dashboardapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,19 +10,25 @@ def index(request):
     '''Creating the home page for fantasy dashboard.'''
     return render(request, 'fantasy_dashboardapp/index.html')
 
+@login_required
 def topics(request):
     '''Show all topics.'''
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'fantasy_dashboardapp/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     '''Show just a single topic'''
     topic = Topic.objects.get(id=topic_id)
+    #Make sure the topic belongs to the current user.
+    if topic.owner != request.user: 
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'fantasy_dashboardapp/topic.html', context)
 
+@login_required
 def new_topic(request):
     '''Adding a new topic'''
     if request.method != 'POST':
@@ -30,13 +38,16 @@ def new_topic(request):
         #POST data submmitted then process 
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit = False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('fantasy_dashboardapp:topics')
     
     # Display a blank or invalid form 
     context = {'form': form}
     return render(request, 'fantasy_dashboardapp/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     '''Add a new entry for a particular topic.'''
     topic = Topic.objects.get(id=topic_id)
@@ -57,10 +68,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'fantasy_dashboardapp/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     '''Edit an existing entry.'''
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user: 
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
